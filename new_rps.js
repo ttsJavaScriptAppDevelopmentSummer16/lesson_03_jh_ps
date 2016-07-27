54ccfb801539574ed1762f735e373a2dd65cf9b1
Jude Harding <jharding@genesisgroupusa.com>
2016-07-26 22:53:36 -0400

    maybe the hands needs more work

--- new_rps.js
+++ new_rps.js
@@ -1,7 +1,7 @@
 var hands = ['rock', 'paper', 'scissors'];
 
 function gethand(){
-    return hands[parseInt(Math.random()*10%3)]
+    return hands[parseInt(Math.random()*10%3)];
 }
 
 console.log(gethand());
@@ -15,3 +15,21 @@ var player2 = {
     name: "Priyam",
     hand: gethand()
 };
+var playRound = function(player1, player2){
+ 	player1.hand();
+ 	player2.hand();
+
+// hands but not tracking rounds yet
+
+  if (player1.hand === player2.hand){
+    console.log("It's a tie - no winner");
+} else if ((player1.hand === 0 && player2.hand === 2) ||
+    (player1.hand === 1 && player2.hand === 0) ||
+    (player1.hand === 2 && player2.hand === 1)) {
+      player1score = player1score +1;
+      console.log( player1.name + " wins");
+    }   else {
+      player2score = player2score +1;
+      console.log( player2.name + " wins");
+    }
+}
