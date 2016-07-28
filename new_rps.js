4bfe313cf62f7869ab5de40ddffbf3f2a35c5f07
Jude Harding <jharding@genesisgroupusa.com>
2016-07-28 16:08:20 -0400

    finished logic but having problem with a function

--- new_rps.js
+++ new_rps.js
@@ -6,7 +6,6 @@ function gethand(){
     return hands[parseInt(Math.random()*10%3)];
 }
 
-
 console.log("Testing Math Random =  " + gethand());
 
 //Define two objects for two players. Each player has name and getHand() properties.
@@ -25,28 +24,43 @@ console.log("Testing Player " + player2.name + " Hand  =  " + player2.hand);
 
 //Define a function called playRound() that Takes two player objects as arguments
 var playRound = function(player1, player2){
- 	player1.hand();
- 	player2.hand();
+    player1.hand();
+    player2.hand();
 };
 console.log("Testing Player Hands  =  " + player1.hand + " VS. " + player2.hand);
 
 var player1score = 0;
 var player2score = 0;
 
-//Gets hands from each and Determines the winner
+//looping for cumulative scores
 
-  if (player1.hand === player2.hand){
+
+  //Gets hands from each and Determines the winner
+  playRound();
+
+if
+( //condition player1 wins
+    (player1.hand === 0 && player2.hand === 2) ||
+    (player1.hand === 1 && player2.hand === 0) ||
+    (player1.hand === 2 && player2.hand === 1)
+)
+{
+    player1score = player1score + 1;
+    console.log( player1.name + " wins ");
+
+} else if
+(
+    (player2.hand === 0 && player1.hand === 2) ||
+    (player2.hand === 1 && player1.hand === 0) ||
+    (player2.hand === 2 && player1.hand === 1)
+)
+{// player2 wins
+    player2score = player2score + 1;
+    console.log( player2.name + " wins ");
+} else
+{
     console.log("It's a tie - no winner");
-  } else if (
-      (player1.hand === 0 && player2.hand === 2) ||
-      (player1.hand === 1 && player2.hand === 0) ||
-      (player1.hand === 2 && player2.hand === 1)
-  ) {
+}
 
-      player1score = player1score +1;
-      console.log( player1.name + " wins");
-
-    } else {
-      player2score = player2score +1;
-      console.log( player2.name + " wins");
-  }
+console.log( player1.name + " has won " +  player1score  + " games.");
+console.log( player2.name + " has won " +  player2score  + " games.");
