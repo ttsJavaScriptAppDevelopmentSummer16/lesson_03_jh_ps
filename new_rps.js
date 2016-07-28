4c94a462f215e2576097a5cdddc976e9398fcb05
Jude Harding <jharding@genesisgroupusa.com>
2016-07-28 13:40:54 -0400

    redoing

--- new_rps.js
+++ new_rps.js
@@ -1,35 +1,52 @@
+// Define a hands array with the values 'rock', 'paper', and 'scissors';
 var hands = ['rock', 'paper', 'scissors'];
 
+//Define a function called getHand() that returns a hand from the array using parseInt(Math.random()*10)%3
 function gethand(){
     return hands[parseInt(Math.random()*10%3)];
 }
 
-console.log(gethand());
+console.log("Testing Math Random =  " + gethand());
 
+//Define two objects for two players. Each player has name and getHand() properties.
 var player1 = {
-    name: "Judy",
+    name: "Jude",
     hand: gethand()
 };
+console.log("Testing Player " + player1.name + " Hand  =  " + player1.hand);
+
 
 var player2 = {
     name: "Priyam",
     hand: gethand()
 };
+console.log("Testing Player " + player2.name + " Hand  =  " + player2.hand);
+
+//Define a function called playRound() that Takes two player objects as arguments
 var playRound = function(player1, player2){
  	player1.hand();
  	player2.hand();
+};
+console.log("Testing Player Hands  =  " + player1.hand + " VS. " + player2.hand);
 
-// hands but not tracking rounds yet
+var player1score = 0;
+var player2score = 0;
+
+//Gets hands from each and Determines the winner
 
   if (player1.hand === player2.hand){
     console.log("It's a tie - no winner");
-} else if ((player1.hand === 0 && player2.hand === 2) ||
-    (player1.hand === 1 && player2.hand === 0) ||
-    (player1.hand === 2 && player2.hand === 1)) {
+  } else if (
+      (player1.hand === 0 && player2.hand === 2) ||
+      (player1.hand === 1 && player2.hand === 0) ||
+      (player1.hand === 2 && player2.hand === 1)
+  ) {
+
       player1score = player1score +1;
       console.log( player1.name + " wins");
-    }   else {
+
+    } else {
       player2score = player2score +1;
       console.log( player2.name + " wins");
-    }
-}
+  }
+ 
--- ps_rps.js
+++ ps_rps.js
@@ -1,67 +1,40 @@
+var hands = ['rock', 'paper', 'scissors'];
+
+function gethand(){
+    return hands[parseInt(Math.random()*10%3)];
+}
+
+//console.log(gethand());
+
 var player1 = {
     name: "Judy",
-    hand: function(){
-      var weapons = ['rock', 'paper', 'scissors'];
-      return weapons[parseInt(Math.random()*10)%3];
-  }
+    hand: gethand()
 };
 
 var player2 = {
     name: "Priyam",
-    hand: function(){
-      var weapons = ['rock', 'paper', 'scissors'];
-      return weapons[parseInt(Math.random()*10)%3];
-    }
+    hand: gethand()
 };
 
 function playAround(participant1, participant2){
-  var handParticipant1 = participant1.hand();
-  var handParticipant2 = participant2.hand();
+  handParticipant1 = participant1.hand;
+  handParticipant2 = participant2.hand;
 
   if (handParticipant1 === handParticipant2){
-    console.log('\n' + 'It\'s a Tie!');
-    return null;
+    console.log('It\'s a Tie!')
+    return null
   }else if ((handParticipant1 === 'rock' && handParticipant2 === 'scissors') || (handParticipant1 === 'scissors' && handParticipant2 === 'paper') || (handParticipant1 === 'paper' && handParticipant2 === 'rock'))
   {
-    console.log('\n' + participant1.name + ' chooses ' + handParticipant1);
-    console.log(participant2.name + ' chooses ' + handParticipant2);
-    console.log('\n' + participant1.name + ' wins the round');
-    return participant1;
+    console.log(participant1.name + ' chooses ' + handParticipant1)
+    console.log(participant2.name + ' chooses ' + handParticipant2)
+    console.log('\n' + participant1.name + ' wins!')
+    return participant1
   }else {
-    console.log('\n' + participant1.name + ' chooses ' + handParticipant1);
-    console.log(participant2.name + ' chooses ' + handParticipant2);
-    console.log('\n' + participant2.name + ' wins the round');
-    return participant2;
+    console.log(participant1.name + ' chooses ' + handParticipant1)
+    console.log(participant2.name + ' chooses ' + handParticipant2)
+    console.log('\n' + participant2.name + ' wins!')
+    return participant2
   }
 }
 
-// console.log(playAround(player1,player2));
-// console.log(playAround(player1,player2));
-// console.log(playAround(player1,player2));
-// console.log(playAround(player1,player2));
-
-function playGame(participant1,participant2,playUntil){
-  var scoreParticipant1 = 0;
-  var scoreParticipant2 = 0;
-  while (scoreParticipant1 < playUntil && scoreParticipant2 < playUntil){
-    winner = playAround(participant1,participant2);
-    console.log(winner)
-    if (winner === participant1){
-      scoreParticipant1 += 1;
-    }else if (winner === participant2) {
-      scoreParticipant2 += 1;
-    }else {
-    }
-  }
-  switch (scoreParticipant1 > scoreParticipant2) {
-    case true:
-      console.log('\n' + participant1.name + ' wins the game!')
-      return participant1;
-    case false:
-      console.log('\n' + participant2.name + ' wins the game!')
-      return participant2;
-    default:
-  }
-}
-
-console.log(playGame(player1,player2,5));
+console.log(playAround(player1,player2));
